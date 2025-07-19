dd2a39e43618769fc778650f4b5b471039f4fc7a
Henrykioko <132352158+Henrykioko@users.noreply.github.com>
2025-07-19 11:57:56 +0300

    Update infogathering.sh

--- infogathering/infogathering.sh
+++ infogathering/infogathering.sh
@@ -5,6 +5,16 @@ echo "finding the ipaddress of $url "
 nslookup "$url"
 echo "the ipaddress is shown above 👆👆"
 read -p "enter the ipaddress given: " address
+
+echo "Finding information about the server....."
+whois "$address"
+
+echo " "
+
+echo "finding more info about the website......"
+whatweb "$url" -v
+
 echo "scanning the ports..............."
 nmap -sV --min-rate 5000 "$address"
 echo "COMPLETE"
+
